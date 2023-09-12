2eade814e873b18bfd78d7f8fd6ff458d8e53253
Franky <frank.jr.619@gmail.com>
2023-09-12 14:33:15 +0700

    setup depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
+    kotlin("kapt")
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    id("io.realm.kotlin")
+//    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.fjr619.diary"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fjr619.diary"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +37,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -40,7 +46,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -51,10 +57,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -66,4 +72,56 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.2")
+
+    // Firebase
+//    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+//    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+
+    // Room components
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt ("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+
+    // Runtime Compose
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+
+    // Splash API
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    // Mongo DB Realm
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
+    implementation ("io.realm.kotlin:library-sync:1.10.0")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Coil
+    implementation ("io.coil-kt:coil-compose:2.3.0")
+
+    // Pager - Accompanist
+    implementation ("com.google.accompanist:accompanist-pager:0.27.0")
+
+    // Date-Time Picker
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:core:1.0.2")
+
+    // CALENDAR
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
+
+    // CLOCK
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:clock:1.0.2")
+
+    // Message Bar Compose
+    implementation ("com.github.stevdza-san:MessageBarCompose:1.0.5")
+
+            // One-Tap Compose
+    implementation ("com.github.stevdza-san:OneTapCompose:1.0.3")
+
+
+            // Desugar JDK
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.3")
 }
--- app/src/main/java/com/fjr619/diary/MainActivity.kt
+++ app/src/main/java/com/fjr619/diary/MainActivity.kt
@@ -3,13 +3,18 @@ package com.fjr619.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.fjr619.diary.ui.theme.DiaryTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +22,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiaryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = "main tidak ada",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp)
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiaryTheme {
-        Greeting("Android")
     }
 }
