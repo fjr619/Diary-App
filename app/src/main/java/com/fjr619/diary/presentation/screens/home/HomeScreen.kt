84054fbcde5cf9777448308909be8835c4966f2b
Franky <frank.jr.619@gmail.com>
2023-09-14 10:05:29 +0700

    create diary and mood model classes

--- app/src/main/java/com/fjr619/diary/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/fjr619/diary/presentation/screens/home/HomeScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FloatingActionButton
@@ -19,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -87,9 +87,12 @@ fun NavigationDrawer(
                 )
 
                 NavigationDrawerItem(
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding),
                     label = {
-                        Row(modifier = Modifier.padding(horizontal = 12.dp)
-                        , verticalAlignment = Alignment.CenterVertically) {
+                        Row(
+                            modifier = Modifier.padding(horizontal = 12.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
                             Icon(
                                 painter = painterResource(id = R.drawable.google_logo),
                                 contentDescription = "Google Logo",
@@ -112,7 +115,7 @@ fun NavigationDrawer(
 
 @Composable
 @Preview
-fun NavigationDrawerPreview(){
+fun NavigationDrawerPreview() {
     NavigationDrawer(
         drawerState = rememberDrawerState(initialValue = DrawerValue.Open),
         onSignoutClicked = { /*TODO*/ }) {
